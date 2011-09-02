099421fc86cc59c1c04ecd72b0a3efb41c249d15
John Casey <jdcasey@commonjava.org>
2011-09-02 16:45:57 -0400

    reapply license headers, and add convenience method to CouchRealm to configure the security manager

--- src/main/java/org/commonjava/auth/shiro/couch/CouchRealm.java
+++ src/main/java/org/commonjava/auth/shiro/couch/CouchRealm.java
@@ -1,10 +1,31 @@
+/*******************************************************************************
+ * Copyright (C) 2011  John Casey
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.  If not, see 
+ * <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package org.commonjava.auth.shiro.couch;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -12,7 +33,9 @@ import org.apache.shiro.authc.SimpleAccount;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.Permission;
+import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.realm.Realm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.commonjava.auth.couch.data.UserDataException;
 import org.commonjava.auth.couch.data.UserDataManager;
@@ -31,12 +54,40 @@ public class CouchRealm
     @Inject
     private UserDataManager dataManager;
 
+    @Inject
+    private CouchPermissionResolver resolver;
+
     CouchRealm()
     {}
 
-    public CouchRealm( final UserDataManager dataManager )
+    public CouchRealm( final UserDataManager dataManager, final CouchPermissionResolver resolver )
     {
         this.dataManager = dataManager;
+        this.resolver = resolver;
+
+        initResolver();
+    }
+
+    public void setupSecurityManager( final Realm... fallbackRealms )
+    {
+        List<Realm> realms = new ArrayList<Realm>();
+        realms.add( this );
+        for ( Realm realm : fallbackRealms )
+        {
+            if ( realm != null )
+            {
+                realms.add( realm );
+            }
+        }
+
+        SecurityUtils.setSecurityManager( new DefaultSecurityManager( realms ) );
+    }
+
+    @PostConstruct
+    protected void initResolver()
+    {
+        setRolePermissionResolver( resolver );
+        setPermissionResolver( resolver );
     }
 
     @Override
--- src/test/java/org/commonjava/auth/shiro/couch/CouchRealmTest.java
+++ src/test/java/org/commonjava/auth/shiro/couch/CouchRealmTest.java
@@ -1,10 +1,26 @@
+/*******************************************************************************
+ * Copyright (C) 2011  John Casey
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.  If not, see 
+ * <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package org.commonjava.auth.shiro.couch;
 
 import static org.commonjava.auth.shiro.couch.fixture.LoggingFixture.setupLogging;
 
 import org.apache.log4j.Level;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.subject.Subject;
 import org.commonjava.auth.couch.conf.DefaultUserManagerConfig;
 import org.commonjava.auth.couch.data.PasswordManager;
@@ -50,13 +66,10 @@ public class CouchRealmTest
 
         manager = new UserDataManager( config, couch );
 
-        realm = new CouchRealm( manager );
-
         CouchPermissionResolver resolver = new CouchPermissionResolver( manager );
-        realm.setRolePermissionResolver( resolver );
-        realm.setPermissionResolver( resolver );
+        realm = new CouchRealm( manager, resolver );
 
-        SecurityUtils.setSecurityManager( new DefaultSecurityManager( realm ) );
+        realm.setupSecurityManager();
     }
 
     @Before
